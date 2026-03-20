f41bbf537da820c589bd368a90c74d96fc4d6ac2
Berkan Eriş <berkan.eris.10@gmail.com>
2026-03-20 16:50:52 +0300

    edited sh file

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -ex
 
-# k6'nın en son sürümünü GitHub'dan (veya uygun bir kaynaktan) indiriyoruz
-# Sistem yetkisi gerekmez, sadece bulunduğumuz klasöre indiririz
+# 1. k6'nın Linux için hazır çalışan dosyasını indiriyoruz (Sistem yetkisi istemez)
 curl -L https://github.com/grafana/k6/releases/download/v0.49.0/k6-v0.49.0-linux-amd64.tar.gz -o k6.tar.gz
 
-# Arşivi açıyoruz
+# 2. Arşivi açıyoruz
 tar -xvzf k6.tar.gz
 
-# Klasörün içindeki k6 dosyasını ana dizine alıyoruz
+# 3. Klasörün içindeki k6 dosyasını bulunduğumuz yere kopyalıyoruz
 cp k6-v0.49.0-linux-amd64/k6 .
 
-# Çalıştırma yetkisi veriyoruz
+# 4. Dosyaya çalışma yetkisi veriyoruz
 chmod +x k6
